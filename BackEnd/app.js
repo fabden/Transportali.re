c5c53bf1c7fadf2caecce3999f5304ca63fa636c
FabDen <fabden@gmail.com>
2021-04-13 10:02:36 +0200

    connection base de donnees mode async

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,7 +1,21 @@
 // Imports
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connection a la base de donnees
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/transportali', { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connexion à MongoDB réussie !');
+  } catch (err) {
+    console.log('Connexion à MongoDB échouée !', err);
+  }
+};
+connectDB();
+
 
 // export
 module.exports = app;
