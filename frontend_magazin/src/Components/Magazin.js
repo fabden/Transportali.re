e0c4b468e72f68e776363211b958655f10889da0
FabDen <fabden@gmail.com>
2021-05-09 15:29:04 +0200

    ajout material ui

---
+++ frontend_magazin/src/Components/Magazin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+
+function Magazin() {
+
+
+    return (
+        <Container >
+            jjhjh
+        </Container>
+    )
+};
+
+export default Magazin
