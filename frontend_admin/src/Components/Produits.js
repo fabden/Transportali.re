75d6bdaab55b91513f5f8f12685de2bb1bbb610f
FabDen <fabden@gmail.com>
2021-05-22 18:36:09 +0200

    ajouts modal produits

--- frontend_admin/src/Components/Produits.js
+++ frontend_admin/src/Components/Produits.js
@@ -7,7 +7,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button, Grid } from '@material-ui/core';
+import { Button, Fade, Grid, Modal } from '@material-ui/core';
+import Backdrop from '@material-ui/core/Backdrop';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -39,15 +40,35 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   table: {
-    minWidth: '65vw',
+    minWidth: '65vw',    
   },
-});
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Produits() {
 
-   const classes = useStyles();       
+   const classes = useStyles();    
+   const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };   
 
     return (
         <Grid container>
@@ -57,10 +78,10 @@ function Produits() {
                         <TableHead>
                         <TableRow>
                             <StyledTableCell>Nom produits</StyledTableCell>
-                            <StyledTableCell align="right">longeurs</StyledTableCell>
-                            <StyledTableCell align="right">Largeurs</StyledTableCell>
-                            <StyledTableCell align="right">Hauteurs</StyledTableCell>
-                            <StyledTableCell align="right">Poids</StyledTableCell>
+                            <StyledTableCell align="right">longeurs (cm)</StyledTableCell>
+                            <StyledTableCell align="right">Largeurs (cm)</StyledTableCell>
+                            <StyledTableCell align="right">Hauteurs (cm)</StyledTableCell>
+                            <StyledTableCell align="right">Poids (Kg)</StyledTableCell>
                             <StyledTableCell align="center">Actions</StyledTableCell>
                         </TableRow>
                         </TableHead>
@@ -82,8 +103,27 @@ function Produits() {
                 </TableContainer>
             </Grid>
             <Grid  xs alignItems="center">
-                <Button color="primary" variant="contained">Nouveaux</Button>
+                <Button color="primary" variant="contained" onClick={handleOpen}>Nouveaux</Button>
             </Grid>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                <div className={classes.paper}>
+                    <h2 id="transition-modal-title">Transition modal</h2>
+                    <p id="transition-modal-description">react-transition-group animates me.</p>
+                </div>
+                </Fade>
+            </Modal>
         </Grid>
     );
 }
